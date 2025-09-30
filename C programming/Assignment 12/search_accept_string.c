8ccc39a33a3011ca02e9d5f265e82bb1c59564e2
Utkarsh Barapatre <131602781+Utkarshbarapatre@users.noreply.github.com>
2025-09-30 21:41:02 +0530

    Add files via upload

---
+++ C programming/Assignment 12/search_accept_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<strings.h>
+void main(){
+	char str[30];
+	char ch;
+	printf("Enter a string:");
+	scanf("%s",str);
+	printf("Enter a character to search in String:");
+	fflush(stdin);
+	scanf("%c",&ch);	
+	char* ptr=strchr(str,ch);
+	printf("\nFound Character %c at:%s",ch,ptr);
+	
+}
