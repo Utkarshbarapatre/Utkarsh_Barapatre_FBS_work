865e01467cdea178712bf3d295713901429f58c7
Utkarsh Barapatre <131602781+Utkarshbarapatre@users.noreply.github.com>
2025-09-30 22:04:29 +0530

    Add files via upload

---
+++ C programming/Assignment 13/Q4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main(){
+	int size;
+	printf("Enter the size of the array:");
+	scanf("%d",&size);	
+	int* arr=(int*)malloc(sizeof(int)*size);
+	int i;
+	printf("Enter the elements in arr:");
+	for(i=0;i<size;i++){
+		printf("\narr[%d]=",i+1);
+		scanf("%d",&arr[i]);
+	}
+	printf("The Even Numbers are:- ");
+	for(i=0;i<size;i++){
+		if(isEven(arr[i])){
+			printf("%d ",arr[i]);
+		}
+	}
+	printf("\nThe Odd Numbers are:- ");
+	for(i=0;i<size;i++){
+		if(isEven(arr[i])==0){
+			printf("%d ",arr[i]);
+		}
+	}
+	
+}
+int isEven(int num){
+	if(num%2==0){
+		return 1;
+	}
+	return 0;
+}
